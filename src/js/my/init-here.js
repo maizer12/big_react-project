7178a787f83b5ce6264db02db61f4a0d3aa68896
vasil <vasilbazhiv12@gmail.com>
2023-05-24 21:55:26 +0300

    new-page

--- src/js/my/init-here.js
+++ src/js/my/init-here.js
@@ -1,3 +1,23 @@
+//
 $(document).ready(function() {
   $('.select2').select2();
 });
+//
+$(".js-range-slider").ionRangeSlider({
+	type: "single",
+	grid: false
+});
+//SLICK
+$(document).ready(function(){
+  $('.car-park-slide').slick({
+    slidesToShow: 3,
+		prevArrow: '.slick-prev',
+    nextArrow: '.slick-next'
+  });
+});
+//accordions
+$( function() {
+	$( "#accordion" ).accordion({
+		active:1000
+	});
+} );
