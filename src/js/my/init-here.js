2e6f2d7f33ae7568c832edf0dc56cf39326c0993
vasil <vasilbazhiv12@gmail.com>
2023-05-25 21:51:56 +0300

    new-adapt.v1

--- src/js/my/init-here.js
+++ src/js/my/init-here.js
@@ -22,3 +22,34 @@ $( function() {
 		collapsible:true
 	});
 } );
+//burger - popup
+const btnBurg = document.querySelector('.header__burger')
+const body = document.querySelector('body')
+const btnPopup = document.querySelector('.start-mani')
+const innerPopup = document.querySelector('.earnings-column')
+const closePopup = document.querySelector('.earnings-column__close')
+
+const clearBody = () =>{
+	if(body){
+		body.classList = ''
+	}
+}
+
+btnBurg?.addEventListener('click', ()=>{
+	body?.classList.toggle('menu-open')
+})
+
+btnPopup?.addEventListener('click', ()=>{
+	body?.classList.toggle('popup-open')
+})
+
+innerPopup?.addEventListener('click',(e)=>{
+	const elem = e.target.classList.contains('earnings-column')
+	if(elem){
+		body?.classList.toggle('popup-open')
+	}
+})
+
+closePopup?.addEventListener('click', ()=>{
+	body?.classList.toggle('popup-open')
+})
