a45051d22a4c91cfb56c53a547e7309f8ac11061
vasil <vasilbazhiv12@gmail.com>
2023-05-26 16:16:43 +0300

    new-change-add

--- src/js/my/init-here.js
+++ src/js/my/init-here.js
@@ -1,55 +1,99 @@
 //
-$(document).ready(function() {
-  $('.select2').select2();
-});
+$(document).ready(function () {
+	$('.select2').select2()
+})
 //
-$(".js-range-slider").ionRangeSlider({
-	type: "single",
-	grid: false
-});
+$('.js-range-slider').ionRangeSlider({
+	type: 'single',
+	grid: false,
+})
 //SLICK
-$(document).ready(function() {
-	var slider = $(".car-park-slide");
-	var slidesToShow = calculateSlidesToShow();
+$(document).ready(function () {
+	var slider = $('.car-park-slide')
+	var slidesToShow = calculateSlidesToShow()
 
 	slider.slick({
 		slidesToShow: slidesToShow,
 		prevArrow: '.slick-prev',
-		nextArrow: '.slick-next'
+		nextArrow: '.slick-next',
 		// Додайте інші налаштування Slick Slider, які вам потрібні
-	});
+	})
 
 	function calculateSlidesToShow() {
 		// Визначте ширину екрану або контейнера, в якому відображається слайдер
-		var containerWidth = slider.width();
+		var containerWidth = slider.width()
 
 		// Перевірте, чи ширина менше 600
 		if (containerWidth < 600) {
-			return 1; // Показувати лише 1 слайд
+			return 1 // Показувати лише 1 слайд
 		}
 
 		// Визначте ширину одного слайда
-		var slideWidth = slider.find("div").first().outerWidth();
+		var slideWidth = slider.find('div').first().outerWidth()
 
 		// Обчисліть кількість слайдів, які можуть вміститися на екрані
-		var slidesToShow = Math.floor(containerWidth / slideWidth);
+		var slidesToShow = Math.floor(containerWidth / slideWidth)
 
-		return slidesToShow;
+		return slidesToShow
 	}
 
 	// Оновити кількість слайдів, коли розмір вікна браузера змінюється
-	$(window).resize(function() {
-		var slidesToShow = calculateSlidesToShow();
-		slider.slick("slickSetOption", "slidesToShow", slidesToShow);
-	});
-});
+	$(window).resize(function () {
+		var slidesToShow = calculateSlidesToShow()
+		slider.slick('slickSetOption', 'slidesToShow', slidesToShow)
+	})
+})
 //accordions
-$( function() {
-	$( ".often-questions__acordions" ).accordion({
-		active:1000,
-		collapsible:true
-	});
-} );
+// $(function() {
+//   $(".often-questions__acordions").accordion({
+//     active: 1000,
+//     collapsible: true,
+//     activate: function(event, ui) {
+//       if (ui.newPanel.length > 0) {
+// 				ui.newPanel.parent().addClass("active-accord-parent");
+//         $("body").addClass("active-accord-class");
+//       } else {
+//         $("body").removeClass("active-accord-class");
+//       }
+//     }
+//   });
+// });
+$(function() {
+  function toggleAccordionClass() {
+    var windowWidth = $(window).width();
+    var isMobileScreen = windowWidth < 500;
+
+    if (isMobileScreen) {
+      $(".often-questions__acordions").accordion({
+        active: 1000,
+        collapsible: true,
+        activate: function(event, ui) {
+          if (ui.newPanel.length > 0) {
+            ui.newPanel.parent().addClass("active-accord-parent"); 
+            $("body").addClass("active-accord-class");
+          } else {
+						const active = document.querySelector('.active-accord-parent')
+						active.classList.remove("active-accord-parent"); 
+            $("body").removeClass("active-accord-class");
+          }
+        }
+      });
+    } else {
+      $(".often-questions__acordions").accordion({
+        active: false,
+        collapsible: true
+      });
+      $("body").removeClass("active-accord-class"); 
+    }
+  }
+
+  toggleAccordionClass();
+  
+  $(window).on("resize", function() {
+    toggleAccordionClass(); 
+  });
+});
+
 //burger - popup
 const btnBurg = document.querySelector('.header__burger')
 const body = document.querySelector('body')
@@ -57,27 +101,27 @@ const btnPopup = document.querySelector('.start-mani')
 const innerPopup = document.querySelector('.earnings-column')
 const closePopup = document.querySelector('.earnings-column__close')
 
-const clearBody = () =>{
-	if(body){
+const clearBody = () => {
+	if (body) {
 		body.classList = ''
 	}
 }
 
-btnBurg?.addEventListener('click', ()=>{
+btnBurg?.addEventListener('click', () => {
 	body?.classList.toggle('menu-open')
 })
 
-btnPopup?.addEventListener('click', ()=>{
+btnPopup?.addEventListener('click', () => {
 	body?.classList.toggle('popup-open')
 })
 
-innerPopup?.addEventListener('click',(e)=>{
+innerPopup?.addEventListener('click', e => {
 	const elem = e.target.classList.contains('earnings-column')
-	if(elem){
+	if (elem) {
 		body?.classList.toggle('popup-open')
 	}
 })
 
-closePopup?.addEventListener('click', ()=>{
+closePopup?.addEventListener('click', () => {
 	body?.classList.toggle('popup-open')
 })
